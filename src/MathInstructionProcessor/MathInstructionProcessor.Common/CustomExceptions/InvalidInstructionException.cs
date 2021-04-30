cb1cdee174f1379df034bf2d452593a953dca742
Keith <keith.grima@codit.eu>
2021-04-30 12:41:09 +0200

    added implementation to parse and calculate data

---
+++ src/MathInstructionProcessor/MathInstructionProcessor.Common/CustomExceptions/InvalidInstructionException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathInstructionProcessor.Common.CustomExceptions
+{
+    [Serializable()]
+    public class InvalidInstructionException : System.Exception
+    {
+        public InvalidInstructionException() : base() { }
+        public InvalidInstructionException(string message) : base(message) { }
+        public InvalidInstructionException(string message, System.Exception inner) : base(message, inner) { }
+
+        // A constructor is needed for serialization when an
+        // exception propagates from a remoting server to the client. 
+        protected InvalidInstructionException(System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
